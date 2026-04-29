cc318fb88024cb4a63265b3329d1a4f7cb316795
talha-23 <muhammadtalha8150@gmail.com>
2026-04-29 19:29:15 +0500

    Signed-off-by: talha-23 <muhammadtalha8150@gmail.com>

---
+++ EduConnect/Exceptions/CourseFullException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EduConnect.Exceptions
+{
+    public class CourseFullException : Exception
+    {
+        public CourseFullException()
+            : base("Cannot enroll because the course has reached maximum capacity.")
+        {
+        }
+
+        public CourseFullException(string message)
+            : base(message)
+        {
+        }
+
+        public CourseFullException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ EduConnect/Exceptions/StudentHasActiveEnrollmentsException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EduConnect.Exceptions
+{
+    public class StudentHasActiveEnrollmentsException : Exception
+    {
+        public StudentHasActiveEnrollmentsException()
+            : base("Cannot delete student because they have active course enrollments.")
+        {
+        }
+
+        public StudentHasActiveEnrollmentsException(string message)
+            : base(message)
+        {
+        }
+
+        public StudentHasActiveEnrollmentsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
